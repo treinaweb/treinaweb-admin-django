bfdc2ce014d089e1c2d7de290f5a5cf03e7f51b8
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2023-03-18 14:55:50 -0300

    02.02_Criando_Registrando_Models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Tecnologia
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Tecnologia)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Tecnologia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=300, null=False, blank=False)
+    descricao = models.CharField(max_length=500, null=False, blank=False)
+    conteudo = models.TextField(null=False, blank=False)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    tecnologias = models.ManyToManyField(Tecnologia, blank=False)
+
+    def __str__(self):
+        return self.titulo
