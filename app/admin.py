385cf4e2cdccc7cc12fcca76475dd4e018e9f7c6
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2023-03-21 09:26:31 -0300

    04.01_Ativando_StackedInline

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Post, Tecnologia
+from django.contrib.auth.models import User
 
 # Register your models here.
 
+admin.site.unregister(User)
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 1
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [PostInline, ]
+
+
 admin.site.register(Post)
 admin.site.register(Tecnologia)
+admin.site.register(User, UserAdmin)
