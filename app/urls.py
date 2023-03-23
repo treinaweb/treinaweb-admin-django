cb84cdc93d0b25b37f0dbe83d4ac4ed30c32f165
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2023-03-23 15:22:15 -0300

    05.02_Listando_Post_ID

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("posts", PostListView.as_view(), name="lista_posts")
+    path("posts", PostListView.as_view(), name="lista_posts"),
+    path("post/<int:pk>", PostDetailView.as_view(), name="lista_post"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -9,3 +9,9 @@ class PostListView(ListView):
     model = Post
     context_object_name = "posts"
     template_name = "lista_posts.html"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "lista_post.html"
